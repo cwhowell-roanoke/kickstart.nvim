222b273006b28666f578ef42fad4fd7ef79f23b6
cwhowell <howell@roanoke.edu>
2025-07-17 14:50:28 -0400

    Add omni source, fuzzy uses rust if available and re-enable blink on :

--- init.lua
+++ init.lua
@@ -843,7 +843,7 @@ require('lazy').setup({
       },
 
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'lazydev' },
+        default = { 'lsp', 'path', 'snippets', 'lazydev', 'omni' },
         providers = {
           lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
         },
@@ -858,14 +858,14 @@ require('lazy').setup({
       -- the rust implementation via `'prefer_rust_with_warning'`
       --
       -- See :h blink-cmp-config-fuzzy for more information
-      fuzzy = { implementation = 'lua' },
+      fuzzy = { implementation = 'prefer_rust_with_warning' },
 
       -- Shows a signature help window while you type arguments for a function
       signature = { enabled = true },
 
       -- Temporarily disabled because it's causing immense lag when typing :!
       cmdline = {
-        enabled = false,
+        enabled = true,
       },
     },
   },
