fcc77efa57652f9faaec571efad927d49242459b
howell <howell@roanoke.edu>
2026-07-01 09:50:57 -0400

    Enable dotfiles and .gitignored file in neo-tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -19,6 +19,11 @@ return {
         mappings = {
           ['\\'] = 'close_window',
         },
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
       },
     },
   },
